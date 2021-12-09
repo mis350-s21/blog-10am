cd0798860654dc356b5d76226c9a613e907b1699
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-12-09 12:08:58 +0300

    saving progrss

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','status','created_at')
+    list_filter = ('status', 'created_at', )
+    search_fields = ['title', 'body',]
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+
+    STATUS = (
+        (0, "Unpublished"),
+        (1, "Published"),
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title}: created at {self.created_at}"
