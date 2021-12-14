28d17696671e103da95f7891a80d452c28b7c0a4
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-12-14 12:10:27 +0300

    Update admin.py

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','status','created_at')
+    list_display = ('title','status','created_at', 'updated_at')
     list_filter = ('status', 'created_at', )
     search_fields = ['title', 'body',]
     prepopulated_fields = {'slug': ('title',)}
