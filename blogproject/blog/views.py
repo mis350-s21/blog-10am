ccc95aa95e4463b47f1d4d4a3fbcf03fc0706ced
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-12-23 11:34:07 +0300

    saving progress

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -29,15 +29,20 @@ def unpub_post_list(request):
 
 
 def post_details(request, id):
-    p = Post.objects.get(id=id)
+    # p = Post.objects.get(id=id)
+    p = get_object_or_404(Post, id=id)
     c = {
         'post':p,
     }
     return render(request, 'post_details.html', c)
 
 def post_details_slug(request, s):
+    
     p = Post.objects.get(slug=s)
+    cs = p.comment_set.all()
+
     c = {
         'post':p,
+        'comments': cs,
     }
     return render(request, 'post_details.html', c)
